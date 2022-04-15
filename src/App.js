d455b3b4e960abc57fc358a97ed784ec6c205c8d
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-15 12:46:03 +0000

    react fullstack open part 1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,47 @@
+import { useState } from "react";
 
-const Header = (props) => {
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return <div>the app is used by pressing the buttons</div>;
+    }
+    return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Display = (props) => <div>{props.value}</div>;
+
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>{text}</button>
+);
+const App = () => {
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([]);
+    const [value, setValue] = useState(10);
+
+    const setToValue = (newValue) => {
+        setValue(newValue);
+    };
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat("L"));
+        setLeft(left + 1);
+    };
+    const handleRightClick = () => {
+        setAll(allClicks.concat("R"));
+        setRight(right + 1);
+    };
+
     return (
         <div>
-            <h1>{props.course}</h1>
+            {left}
+            <Button handleClick={handleLeftClick} text="left" />
+            <Button handleClick={handleRightClick} text="right" />
+            {right}
+            <History allClicks={allClicks} />
+            <Display value={value} />
+            <Button handleClick={() => setToValue(1000)} text="1k"/>
         </div>
     );
 };
-const Content = (props) => {
-    return (
-        <div>
-            <p>
-                {props.parts[0].name} {props.parts[0].exercises}
-            </p>
-            <p>
-                {props.parts[1].name} {props.parts[1].exercises}
-            </p>
-            <p>
-                {props.parts[2].name} {props.parts[2].exercises}
-            </p>
-        </div>
-    );
-};
-
-const Total = (props) => {
-    return (
-        <div>
-            <p>
-                Number of exercises{" "}
-                {props.parts[0].exercises +
-                    props.parts[1].exercises +
-                    props.parts[2].exercises}
-            </p>
-        </div>
-    );
-};
-
-function App() {
-    const course = "Half Stack application development";
-    const part1 = "Fundamentals of React";
-    const exercises1 = 10;
-    const part2 = "Using props to pass data";
-    const exercises2 = 7;
-    const part3 = "State of a component";
-    const exercises3 = 14;
-    const content = [
-        { name: part1, exercises: exercises1 },
-        { name: part2, exercises: exercises2 },
-        { name: part3, exercises: exercises3 },
-    ];
-
-    return (
-        <div className="App">
-            <Header course={course} />
-            <Content parts={content} />
-            <Total parts={content} />
-        </div>
-    );
-}
 
 export default App;
