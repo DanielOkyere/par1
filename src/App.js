cb32d26aed6c1624d406ed9531f10549dd9bd20b
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-04-15 13:43:49 +0000

    react fullstack open part 2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
-import { useState } from "react";
+import { useState } from 'react';
+import Note from "./components/Note";
+const App = (props) => {
 
-const History = (props) => {
-    if (props.allClicks.length === 0) {
-        return <div>the app is used by pressing the buttons</div>;
+    const [ notes, setNotes] = useState(props.notes)
+
+    const addNote = (event)=>{
+        event.preventDefault()
+        console.log('button clicked', event.target)
     }
-    return <div>button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Display = (props) => <div>{props.value}</div>;
-
-const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>{text}</button>
-);
-const App = () => {
-    const [left, setLeft] = useState(0);
-    const [right, setRight] = useState(0);
-    const [allClicks, setAll] = useState([]);
-    const [value, setValue] = useState(10);
-
-    const setToValue = (newValue) => {
-        setValue(newValue);
-    };
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat("L"));
-        setLeft(left + 1);
-    };
-    const handleRightClick = () => {
-        setAll(allClicks.concat("R"));
-        setRight(right + 1);
-    };
-
     return (
         <div>
-            {left}
-            <Button handleClick={handleLeftClick} text="left" />
-            <Button handleClick={handleRightClick} text="right" />
-            {right}
-            <History allClicks={allClicks} />
-            <Display value={value} />
-            <Button handleClick={() => setToValue(1000)} text="1k"/>
+            <h1>Notes</h1>
+            <ul>
+                {notes.map((note) => (
+                    <Note key={note.id} note={note} />
+                ))}
+            </ul>
+            <form onSubmit = {addNote}>
+                <input/>
+                <button type="submit">save</button>
+            </form>
         </div>
     );
 };
